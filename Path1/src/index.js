b10aa6a73c4f35d07567fe820862dcc5d0f704e6
Arihant416 <arihantjain416@gmail.com>
2020-05-20 10:55:47 +0530

    Path Init

---
+++ Path1/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Greeting() {
+    return <h1>This is Arihant and this is Path 1 to React Component</h1>;
+}
+
+ReactDOM.render(<Greeting />, document.getElementById('root'));
