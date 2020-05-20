e87bc4ee6b9905bf90ccc3d2cca6fd5194831c9f
Arihant416 <arihantjain416@gmail.com>
2020-05-20 17:59:35 +0530

    Imports and Exports

--- path2/src/App.js
+++ path2/src/App.js
@@ -1,26 +1,35 @@
+// named and default imports and exports
+// only one default export per module
+// think of module as a file
+//rfc functional Component
+//rcc class based Component
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { age, name, person } from './data';
+// import * as data from './data';
+import Banner from './Components/Header/Banner';
+const App = () => {
+    return (
+        <section>
+            <p>This is my content</p>
+            <Banner />
+            <p>{name}</p>
+            <p>{age}</p>
+            <p>{person.name}</p>
+            <p>{person.age}</p>
+        </section>
+    );
+};
+// const App = () => {
+//     return (
+//         <section>
+//             <Banner />
+//             <p>This is my content</p>
+//             <p>{data.name}</p>
+//             <p>{data.age}</p>
+//             <p>{data.person.name}</p>
+//             <p>{data.person.age}</p>
+//         </section>
+//     );
+// };
 
 export default App;
---
+++ path2/src/Components/Header/Banner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { name } from '../../data';
+//rfc
+//rcc
+function Banner() {
+    return (
+        <div>
+            <h1>Hello from Bruce Banner {name}</h1>
+        </div>
+    );
+}
+
+export default Banner;
