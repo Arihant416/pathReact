3c533b2ba09fb19a0920a18b2c013e4c301beb03
Arihant416 <arihantjain416@gmail.com>
2020-05-20 12:02:46 +0530

    Basic Book Component

--- Path1/src/index.js
+++ Path1/src/index.js
@@ -1,8 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-function Greeting() {
-    return <h1>This is Arihant and this is Path 1 to React Component</h1>;
+import './index.css';
+// Stateless functional Component
+// always returns JSX
+function Books() {
+    return (
+        <section className='books'>
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+        </section>
+    );
 }
-
-ReactDOM.render(<Greeting />, document.getElementById('root'));
+const Book = () => {
+    return (
+        <article className='book'>
+            <CoverImage />
+            <Title />
+            <Author />
+        </article>
+    );
+};
+const CoverImage = () => (
+    <img
+        width='300'
+        src='https://images-eu.ssl-images-amazon.com/images/I/41+6Zd6yPgL._AC_US327_FMwebp_QL65_.jpg'
+        alt='Want'
+    />
+);
+const Title = () => <h1 style={{ fontSize: '2rem', color: 'red' }}>Want</h1>;
+const authorStyle = {
+    letterSpacing: '10px',
+    color: 'green',
+};
+const Author = () => <p style={authorStyle}>By Cindy Pon</p>;
+ReactDOM.render(<Books />, document.getElementById('root'));
