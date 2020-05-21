ca7663819fe4f7f33603e4f34f108cd37ee4d045
Arihant416 <arihantjain416@gmail.com>
2020-05-21 22:18:06 +0530

    Class Based example

--- path2/src/App.js
+++ path2/src/App.js
@@ -1,33 +1,38 @@
-// named and default imports and exports
-// only one default export per module
-// think of module as a file
-//rfc functional Component
-//rcc class based Component
-import React from 'react';
-import { age, name, person } from './data';
-// import * as data from './data';
+//Stateless functional Components
+//Class Based Components
+//State , Life cycle Methods
+//React Hoooks
+//Classes - Syntactical Sugar for Constructor Functions
+//Subclass of Component
+//Extends from Component Class
+//Inherits Functionality
+// Component class is used to transfer from basic E6 class
+import React, { Component } from 'react';
+import { name, age, person } from './data';
 import Banner from './Components/Header/Banner';
-const App = () => {
-    return (
-        <section>
-            <p>This is my content</p>
-            <Banner />
-            <p>{name}</p>
-            <p>{age}</p>
-            <p>{person.name}</p>
-            <p>{person.age}</p>
-        </section>
-    );
-};
+class App extends Component {
+    render() {
+        return (
+            <section>
+                <Banner />
+                <p>This is my Content</p>
+                <p>{name}</p>
+                <p>{age}</p>
+                <p>{person.name}</p>
+            </section>
+        );
+    }
+}
+
 // const App = () => {
 //     return (
 //         <section>
-//             <Banner />
 //             <p>This is my content</p>
-//             <p>{data.name}</p>
-//             <p>{data.age}</p>
-//             <p>{data.person.name}</p>
-//             <p>{data.person.age}</p>
+//             <Banner />
+//             <p>{name}</p>
+//             <p>{age}</p>
+//             <p>{person.name}</p>
+//             <p>{person.age}</p>
 //         </section>
 //     );
 // };
