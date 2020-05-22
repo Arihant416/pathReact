7fa2a66f538b978659d41945532aa14efde51e22
Arihant416 <arihantjain416@gmail.com>
2020-05-22 16:51:42 +0530

    Pre-Deploy 2

--- city-tours/src/tourData.js
+++ city-tours/src/tourData.js
@@ -1,34 +1,34 @@
 export const tourData = [
-  {
-    id: 1,
-    city: "new york",
-    img: "./img/newyork.jpeg",
-    name: "new york bridge tour",
-    info:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
-  },
-  {
-    id: 2,
-    city: "paris",
-    img: "./img/paris.jpeg",
-    name: "paris lights tour",
-    info:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
-  },
-  {
-    id: 3,
-    city: "london",
-    img: "./img/london.jpeg",
-    name: "london royal palace tour",
-    info:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
-  },
-  {
-    id: 4,
-    city: "tokyo",
-    img: "./img/tokyo.jpeg",
-    name: "tokyo sushi tour",
-    info:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
-  }
+    {
+        id: 1,
+        city: 'new york',
+        img: './img/newyork.jpeg',
+        name: 'new york bridge tour',
+        info:
+            'New York City received a ninth consecutive annual record of approximately 65.2 million tourists in 2018, counting not just overnight visitors but anyone visiting for the day from over 50 miles away, including commuters.',
+    },
+    {
+        id: 2,
+        city: 'paris',
+        img: './img/paris.jpeg',
+        name: 'paris lights tour',
+        info:
+            'Paris: record tourist numbers in 2017. For the first time ever, Greater Paris exceeded 23 million hotel arrivals (23.6 million - an increase of 11% on 2016 and 5% on 2014). The total number of visitors to Greater Paris, all accommodation combined, exceeded a record 40 million tourists, according to our estimates.',
+    },
+    {
+        id: 3,
+        city: 'london',
+        img: './img/london.jpeg',
+        name: 'london royal palace tour',
+        info:
+            "London is one of the world's leading tourism destinations, and the city is home to an array of famous tourist attractions. The city attracted 20.42 million international visitors in 2018, making it one of the world's most visited in terms of international visits.",
+    },
+    {
+        id: 4,
+        city: 'tokyo',
+        img: './img/tokyo.jpeg',
+        name: 'tokyo sushi tour',
+        info:
+            'Tokyo, Japan’s busy capital, mixes the ultramodern and the traditional, from neon-lit skyscrapers to historic temples. The opulent Meiji Shinto Shrine is known for its towering gate and surrounding woods. The Imperial Palace sits amid large public gardens.',
+    },
 ];
