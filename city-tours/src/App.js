df02a5461b64b783939475a45363a1032b932354
Arihant416 <arihantjain416@gmail.com>
2020-05-22 15:25:34 +0530

    Tourlist Component Structured & Styled

--- city-tours/src/App.js
+++ city-tours/src/App.js
@@ -2,11 +2,14 @@ import React, { Component, Fragment } from 'react';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './App.scss';
 import Navbar from './components/Navbar/Navbar';
+import TourList from './components/TourList';
+
 export class App extends Component {
     render() {
         return (
             <Fragment>
                 <Navbar />
+                <TourList />
             </Fragment>
         );
     }
