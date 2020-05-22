eef2c1ff5310678537024c6a4b714dc7082ace81
Arihant416 <arihantjain416@gmail.com>
2020-05-22 15:01:30 +0530

    Navbar Scss Defined

--- city-tours/src/App.js
+++ city-tours/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import './App.scss';
 import Navbar from './components/Navbar/Navbar';
 export class App extends Component {
