b61744147c0ab845b8a8eab4841f510480ec8be2
Arihant416 <arihantjain416@gmail.com>
2020-05-22 14:38:03 +0530

    Install & Setup

---
+++ city-tours/src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import './App.scss';
+export class App extends Component {
+    render() {
+        return <h1>Our current App</h1>;
+    }
+}
+
+export default App;
