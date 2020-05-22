5204de2b47bf5ab3f10bafe8518b0263b988bfba
Arihant416 <arihantjain416@gmail.com>
2020-05-22 10:35:20 +0530

    Events

--- path2/src/Book.js
+++ path2/src/Book.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 
 export default class Book extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 1,
+        };
+        // this.handleClick = this.handleClick.bind(this);
+    }
+
+    // handleClick() {
+    //     console.log('You clicked me');
+    //     console.log(this.state.count);
+    // }
+    handleClick = () => {
+        console.log('You Clicked Me');
+        console.log(this.state.count);
+    };
     render() {
         //   console.log(this.props);
         const { img, title, author } = this.props.info;
@@ -10,6 +26,9 @@ export default class Book extends Component {
                 <div>
                     <h4>Book Title : {title}</h4>
                     <h6>Author : {author}</h6>
+                    <button type='button' onClick={this.handleClick}>
+                        Add Count
+                    </button>
                 </div>
             </article>
         );
