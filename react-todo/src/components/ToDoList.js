bce7b8ef00c81a234442a1df19ca9292f4f28cc3
Arihant416 <arihantjain416@gmail.com>
2020-05-30 11:23:26 +0530

    ToDo List Component structured

--- react-todo/src/components/ToDoList.js
+++ react-todo/src/components/ToDoList.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react'
 import ToDoItem from './ToDoItem';
 export default class ToDoList extends Component {
    render() {
+      const { items, clearList, handleDelete, handleEdit } = this.props
       return (
-         <div>
-            <h2>ToDo List</h2>
-            <ToDoItem />
-         </div>
+         <ul className="list-group my-5">
+            <h3 className="text-capitalize text-center">todo list</h3>
+            {items.map(item => { return (<ToDoItem key={item.id} title={item.title} handleDelete={() => handleDelete(item.id)} handleEdit={() => handleEdit(item.id)} />) })}
+            <button type="button" className="btn btn-danger btn-block text-uppercase mt-5" onClick={clearList}>Clear list</button>
+         </ul>
       )
    }
 }
