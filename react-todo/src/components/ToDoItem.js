6dec9a2f739c57d42cf22c49b8cfb77bfd0218f4
Arihant416 <arihantjain416@gmail.com>
2020-05-30 12:37:46 +0530

    Styling alternatives

--- react-todo/src/components/ToDoItem.js
+++ react-todo/src/components/ToDoItem.js
@@ -4,7 +4,7 @@ export default class ToDoItem extends Component {
    render() {
       const { title, handleDelete, handleEdit } = this.props
       return (
-         <li className="list-group-item list-group-item-border-rounded text-capitalize d-flex justify-content-between my-2" style={{ border: "rounded" }}>
+         <li className="list-group-item list-group-item-border-rounded text-capitalize d-flex justify-content-between my-2">
             <h6>{title}</h6>
             <div className="todo-icon">
                <span className="mx-2 text-success" onClick={handleEdit}><i className="fa fa-pencil" /></span>
--- react-todo/src/components/ToDoList.js
+++ react-todo/src/components/ToDoList.js
@@ -5,7 +5,11 @@ export default class ToDoList extends Component {
       const { items, clearList, handleDelete, handleEdit } = this.props
       return (
          <ul className="list-group list-group-flush my-5">
-            <h3 className="text-capitalize text-center">todo list</h3>
+            <h3
+               className="text-capitalize text-center"
+               style={{ color: "Purple", fontFamily: "Open Sans" }}>
+               {items.length > 0 ? <div>List of Items<span>😃</span></div> : <div>No items to display<span>🎭</span></div>}
+            </h3>
             {items.map(item => { return (<ToDoItem key={item.id} title={item.title} handleDelete={() => handleDelete(item.id)} handleEdit={() => handleEdit(item.id)} />) })}
             <button type="button" className="btn btn-danger btn-block text-uppercase mt-5" onClick={clearList}>Clear list</button>
          </ul>
